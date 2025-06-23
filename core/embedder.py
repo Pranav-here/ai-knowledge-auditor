b5779f55ff3a69085201150172f77610aa0c7fed
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-23 18:27:04 +0530

    Update embedder.py
    Change to check if some other model works better

--- core/embedder.py
+++ core/embedder.py
@@ -4,7 +4,7 @@ from sentence_transformers import SentenceTransformer
 from transformers import pipeline
 
 def load_embedder():
-    return SentenceTransformer("all-MiniLM-L6-v2", device = "cpu").to("cpu")
+    return SentenceTransformer("sentence-transformers/all-mpnet-base-v2", device = "cpu").to("cpu")
 
 def load_summarizer():
     return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")

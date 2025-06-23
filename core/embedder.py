68d85a1069c50b8af47d9e9136fe0f2a0e079f20
Pranav-here <113384349+Pranav-here@users.noreply.github.com>
2025-06-23 15:59:16 +0530

    Update embedder.py

---
+++ core/embedder.py
@@ -0,0 +1,8 @@
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline
+
+def load_embedder():
+    return SentenceTransformer("all-MiniLM-L6-v2", device = "cpu").to("cpu")
+
+def load_summarizer():
+    return pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
